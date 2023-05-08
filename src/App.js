7abe6bfc2b87bba4949a4b4989a35a3f520f324a
HuyCao <huy.cao@rikai.technology>
2023-05-08 16:10:07 +0700

    Course 5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -30,9 +30,14 @@ const App = () => {
     },
   ]
 
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseDataHandler = expense => {
     console.log("In App.js");
     console.log(expense);
+    setExpenses((prevExpenses) => {
+      return[expense, ...expenses];
+    });
   };
 
   // return React.createElement(
